ea1b8136eff71fe05ae5323df3105e6dc3b1e80a
Caio Santos de Moura <caios6630@gmail.com>
2026-05-03 21:17:43 -0300

    Update calculo-de-media.c

--- calculo-de-media/calculo-de-media.c
+++ calculo-de-media/calculo-de-media.c
@@ -1,1 +1,27 @@
 
+#include <stdio.h>
+
+struct escola{ //ESTRUTURA
+	float n1, n2, media;
+};
+
+int main(){ //OBJETO
+	struct escola estudante[4];
+	//CADASTRAR
+	for(i=1; i<=4; i++;){
+		printf("Digite N1: ");
+		fflush(stdin);
+		scanf("%f", &estudante[i].n1);
+		
+		printf("Digite N2: ");
+		fflush(stdin);
+		scanf("%f", &estudante[i].n2);
+		estudante[1].media = (estudante[1].n1 + estudante[1].n2) /2;
+		system("cls");
+	}
+	
+	//IMPRIMIR
+	for(i=1; i<=4; i++){
+		printf("MEDIA:  %.2f \n", estudante[1].media);
+	}
+}
