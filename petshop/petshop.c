e50bf797838c82231e6234fdfeaf755b990f606d
Caio Santos de Moura <caios6630@gmail.com>
2026-05-03 21:39:16 -0300

    Update petshop.c

--- petshop/petshop.c
+++ petshop/petshop.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+
+int main(){
+	char senha[3], cliente, pet;
+	int telefone;
+	
+	while(1){
+		
+		printf("Digite a senha: ");
+		scanf("%s", &senha);
+		
+		if(strcmp(senha, "123") == 0){
+			printf("SENHA CORRETA\n");
+			break;
+		}
+		else{
+			printf("ERRO DE SENHA\n");
+		}
+	}
+	
+	while(1){
+		
+		printf("CADASTRAR CLIENTE");
+		printf("Digite o nome do cliente: ");
+		printf("Digite o nome do pet: ");
+		printf("Digite o telefone: ");
+		
+	}
+}
+
 
